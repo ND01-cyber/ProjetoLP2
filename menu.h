95964a1230ca8beed806bf271462f336b77428ff
jahnbal <silveirarthurburner@gmail.com>
2026-07-01 17:28:25 -0300

    Delete menu.h

--- menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-void Menu_Principal(int *escolha_principal);
-void Menu_Atendimento(int *escolha_secundaria);
-void Menu_Cadastro(int *escolha_secundaria);
-
-#endif
